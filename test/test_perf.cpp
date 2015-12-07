b0625cc9bef47e367ccd03844099571e097caf2e
Serge Aleynikov <saleyn@gmail.com>
2015-12-07 13:34:03 -0500

    Add perf tests

--- test/test_perf.cpp
+++ test/test_perf.cpp
@@ -6,7 +6,7 @@
 
 using namespace EIXX_NAMESPACE;
 
-int iterations=500000;
+int iterations=200000;
 
 class timer {
     struct rusage start, end;
@@ -14,14 +14,19 @@ class timer {
 public:
     timer() { begin(); }
 
-    void sample(const char* title, bool restart = true) {
+    void restart() { begin(); }
+
+    void sample(const char* title, bool restart = true, size_t out = 0) {
         getrusage(RUSAGE_THREAD, &end);
         double diff = (double)(end.ru_utime.tv_sec  - start.ru_utime.tv_sec +
                                end.ru_stime.tv_sec  - start.ru_stime.tv_sec) +
                       (double)(end.ru_utime.tv_usec - start.ru_utime.tv_usec +
                                end.ru_stime.tv_usec - start.ru_stime.tv_usec)/1000000.0;
 
-        printf("%30s | %.6f (speed: %5.3fus)\n", title, diff, 1000000.0*diff/iterations);
+        // out is used merely to trick the optimizer
+        printf("%30s | latency: %7.3fus, speed: %9ld/s%s", title,
+               1000000.0*diff/iterations, diff > 0 ? long((double)iterations / diff) : 0,
+               out == 0 ? "\n" : " \n");
         if (restart)
             begin();
     }
@@ -40,62 +45,114 @@ int main(int argc, char* argv[]) {
     timer tot;
     timer t;
 
+    size_t size = 0;
     for (int j=0; j < iterations; j++)
-        { eterm(1); }
-    t.sample("Integer");
+        { size += eterm(1).encode_size(); }
+    t.sample("Integer", true, size);
     for (int j=0; j < iterations; j++)
-        { eterm(1.0); }
-    t.sample("Double");
+        { eterm(1.0).encode_size(); }
+    t.sample("Double", true, size);
     for (int j=0; j < iterations; j++)
-        { eterm(true); }
-    t.sample("Bool");
+        { eterm(true).encode_size(); }
+    t.sample("Bool", true, size);
     for (int j=0; j < iterations; j++)
-        { eterm("test"); }
-    t.sample("String");
+        { eterm("test").encode_size(); }
+    t.sample("String", true, size);
     for (int j=0; j < iterations; j++)
-        { atom("test"); }
-    t.sample("Atom1");
+        { atom("test").encode_size(); }
+    t.sample("Atom1", true, size);
     {
         atom a("test");
         int k = 0;
         for (int j=0; j < iterations; j++)
-            { atom t("test"); if (t==a) k++; }
-        t.sample("Atom2");
+            { atom t("test"); if (t==a) k++; size += eterm(t).encode_size(); }
+        t.sample("Atom2", true, size);
     }
     static const char* ss="This is a test string. This is a test string. This is a test string."; 
     for (int j=0; j < iterations; j++) { 
-        binary b(ss, sizeof(ss));
+        binary b(ss, sizeof(ss)); size += eterm(b).encode_size();
     }
-    t.sample("Binary1");
+    t.sample("Binary1", true, size);
     {
         binary b(ss, sizeof(ss));
         for (int j=0; j < iterations; j++) { 
             eterm t(b);
+            size += t.encode_size();
         }
-        t.sample("Binary2");
+        t.sample("Binary2", true, size);
     }
 
     eterm l[] = {
         eterm(10), eterm("atom_value"), eterm(true) };
     for (int j=0; j < iterations; j++)
-        { tuple t(l); }
-    t.sample("Tuple1");
+        { tuple t(l); size += eterm(t).encode_size(); }
+    t.sample("Tuple1", true, size);
     {
         tuple tup(l);
         for (int j=0; j < iterations; j++)
-            { eterm et(tup); }
-        t.sample("Tuple2");
+            { eterm et(tup); size += et.encode_size(); }
+        t.sample("Tuple2", true, size);
     }
     for (int j=0; j < iterations; j++)
-        { list t(l); }
-    t.sample("List1");
+        { list t(l); size += eterm(t).encode_size(); }
+    t.sample("List1", true, size);
     {
         list ll(l);
         for (int j=0; j < iterations; j++)
-            { eterm et(ll); }
-        t.sample("List2");
+            { eterm et(ll); size += et.encode_size(); }
+        t.sample("List2", true, size);
     }
-    tot.sample("Total time");
+
+    static const eterm s_md1 =
+        eterm::format("{md, Xchg, Instr, [{q, [{BPx,BQty}], [{APx, AQty}]}]}");
+    static const eterm s_md2 =
+        eterm::format("{md, Xchg, Instr, [{q, L1, L2}]}");
+    static const atom  am_Xchg ("Xchg");
+    static const atom  am_Instr("Instr");
+    static const atom  am_BPx  ("BPx");
+    static const atom  am_AQty ("AQty");
+    static const atom  am_APx  ("APx");
+    static const atom  am_BQty ("BQty");
+    static const atom  am_L1   ("L1");
+    static const atom  am_L2   ("L2");
+    atom   xchg ("CNX");
+    atom   instr("EUR/USD");
+    t.restart();
+    {
+        size_t size = 0;
+        for (int j=0; j < iterations; j++) {
+            auto x = s_md1.apply({{am_Xchg, xchg},  {am_Instr, instr},
+                                  {am_BPx, 1.2345}, {am_BQty, 100000},
+                                  {am_APx, 1.2355}, {am_AQty, 200000}});
+            size += x.encode_size();
+        }
+        t.sample("Apply speed", true, size);
+    }
+    {
+        size_t size = 0;
+        for (int j=0; j < iterations; j++) {
+            auto x = s_md2.apply({{am_Xchg, xchg},  {am_Instr, instr},
+                                  {am_L1, list::make(tuple::make(1.2345, 100000))},
+                                  {am_L2, list::make(tuple::make(1.2355, 200000))}});
+            size += x.encode_size();
+        }
+        t.sample("Apply/Create speed", true, size);
+    }
+    atom am_md("md");
+    atom am_q ("q");
+    {
+        iterations /= 10;
+        size_t size = 0;
+        for (int j=0, e = iterations; j < e; j++) {
+            auto x = tuple::make(am_md, xchg, instr,
+                        list::make(tuple::make(am_q,
+                                    list::make(tuple::make(1.2345, 100000)),
+                                    list::make(tuple::make(1.2355, 200000)))));
+            size += x.encode_size();
+        }
+        t.sample("Create speed", true, size);
+        iterations *= 10;
+    }
 
     return 0;
 }
